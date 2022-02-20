9019e39daea940295d83719be26e423a12cc4e3f
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-02-20 14:12:45 +0300

    Update NA method

--- opt-hw02/methods.py
+++ opt-hw02/methods.py
@@ -1,15 +1,24 @@
-def minimize_na(f, a, b, L, atol=1e-5, maxfev=100000, return_nfev=False):
+from collections import namedtuple
+
+
+OptimizeResult = namedtuple('OptimizeResult', ['x_min', 'x', 'y', 'nfev'])
+
+
+def minimize_na(f, a, b, L, atol=1e-5, maxfev=100000, full_output=False):
     """
     Найти на на отрезке [`a`, `b`] минимум функции `f` с помощью метода
     ломаных (недифференцируемая целевая функция, априорно заданная оценка
     константы Липшица `L`).
     """
+    def new_point(i):
+        return (x[i] + x[i + 1]) / 2 - (y[i + 1] - y[i]) / (2 * L)
+
     def characteristic(i):
         return (y[i] + y[i + 1]) / 2 - L * (x[i + 1] - x[i]) / 2
 
     x = [a, b]               # точки разбиения интервала
     y = [f(a), f(b)]         # значения функции в этих точках
-    nfev = 2                 # количество вызовов функции `f`
+    x_m = [new_point(0)]     # точки внутри интервалов
     F = [characteristic(0)]  # характеристики интервалов
 
     while True:
@@ -22,27 +31,38 @@ def minimize_na(f, a, b, L, atol=1e-5, maxfev=100000, return_nfev=False):
                 F_min = F[i]
         i = i_min
 
-        # проверим условие остановки
+        # проверим условие остановки по точности
         if x[i + 1] - x[i] <= atol:
             break
 
-        # условие не выполнено => разбиваем интервал на 2 части
-        if nfev > maxfev:  # слишком много итераций -- останавливаем расчёт
+        # проверяем условие остановки по числу итераций
+        if len(y) > maxfev:  # слишком много итераций -- останавливаем расчёт
             raise Exception(
                 f'Решение не сошлось после {maxfev} вызовов целевой функции.')
-        x_mid = (x[i] + x[i + 1]) / 2 - (y[i + 1] - y[i]) / (2 * L)
-        y_mid = f(x_mid)
-        nfev += 1
+
+        # условие не выполнено => разбиваем интервал на 2 части
+        x.insert(i + 1, x_m[i])
+        y.insert(i + 1, f(x_m[i]))
 
         # меняем характеристику старого интервала на характеристики двух новых
+        x_m.pop(i)
         F.pop(i)
-        x.insert(i + 1, x_mid)
-        y.insert(i + 1, y_mid)
+        x_m.insert(i, new_point(i))
+        x_m.insert(i + 1, new_point(i + 1))
         F.insert(i, characteristic(i))
         F.insert(i + 1, characteristic(i + 1))
 
     # возвращаем результат
     x_min = (x[i] + x[i + 1]) / 2 - (y[i + 1] - y[i]) / (2 * L)
-    if return_nfev:
-        return x_min, nfev
-    return x_min
+    if not full_output:
+        return x_min
+    x_full = []
+    y_full = []
+    for i in range(len(y) - 1):
+        x_full.append(x[i])
+        x_full.append(x_m[i])
+        y_full.append(y[i])
+        y_full.append(F[i])
+    x_full.append(x[-1])
+    y_full.append(y[-1])
+    return OptimizeResult(x_min, x_full, y_full, len(y))
--- opt-hw02/test.py
+++ opt-hw02/test.py
@@ -1,13 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from test_functions import f1
 from methods import minimize_na
 
 
+# minimizer settings
 a = -1.5
 b = 11
-# L = 7536
-L = 8500
+L = 13900
 eps = 1e-4 * (b - a)
 
+# solve the optimization problem
+ans = minimize_na(f1, a, b, L, atol=eps, full_output=True)
+print(f'Минимум: {ans.x_min:.3f}'
+      + f', количество вызовов целевой функции: {ans.nfev}')
 
-ans, nfev = minimize_na(f1, a, b, L, atol=eps, return_nfev=True)
-print(ans, nfev)
+# plot results
+x = np.linspace(a, b, 200)
+y = [f1(xi) for xi in x]
+plt.figure()
+plt.plot(x, y)
+plt.plot(ans.x, ans.y, 'r-', lw=0.5)
+plt.show()
