8ce041e02c0a290040c3da80549ef4cde0755284
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-03-14 20:13:46 +0300

    Add test functions

---
+++ opt-hw03/visualize.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from test_functions import quadratic, rosenbrock
+
+
+mesh = np.meshgrid(np.linspace(-5, 5), np.linspace(-5, 5))
+x1 = np.linspace(-3, 5)
+x2 = np.linspace(-2, 3)
+mesh = np.meshgrid(x1, x2)
+
+
+fig = plt.figure('Quadratic function')
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(*mesh, quadratic(*mesh))
+
+fig2 = plt.figure('Rosenbrock function')
+ax2 = fig2.add_subplot(projection='3d')
+ax2.plot_surface(*mesh, rosenbrock(*mesh))
+plt.show()
