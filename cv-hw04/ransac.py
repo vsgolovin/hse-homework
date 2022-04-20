c14d3e2767ccd43a454eaf6057000e8646b58b63
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-04-20 11:26:55 +0300

    Update RANSAC to find 3x3 matrix

--- cv-hw04/ransac.py
+++ cv-hw04/ransac.py
@@ -24,7 +24,7 @@ def main():
                                        matches, 4)
     print(M)
     print(status.sum())
-    H = findHomography(kpsA, kpsB, matches, 4, 5.0)
+    H = findHomography(kpsA, kpsB, matches, 4, 5.0, 100, True)
     print(H)
 
     width = trainImg.shape[1] + queryImg.shape[1]
@@ -41,7 +41,9 @@ def findHomography(kpsA: List[cv2.KeyPoint],
                    kpsB: List[cv2.KeyPoint],
                    matches: List[cv2.DMatch],
                    s: int = 4,
-                   reprojThresh: float = 4.0):
+                   reprojThresh: float = 4.0,
+                   num_iterations: int = 100,
+                   use_all: bool = False):
     if len(matches) <= s:
         return None
 
@@ -49,45 +51,67 @@ def findHomography(kpsA: List[cv2.KeyPoint],
     ptsA = np.float32([kpsA[m.queryIdx].pt for m in matches])
     ptsB = np.float32([kpsB[m.trainIdx].pt for m in matches])
 
-    return ransac(ptsA, ptsB, s, reprojThresh)
+    return ransac(ptsA, ptsB, s, reprojThresh, num_iterations, use_all)
 
 
-def ransac(ptsA: np.ndarray, ptsB: np.ndarray, s: int, reprojThresh: float,
-           num_iterations: int = 100):
+def ransac(ptsA: np.ndarray, ptsB: np.ndarray, s: int = 4,
+           reprojThresh: float = 4.0, num_iterations: int = 100,
+           use_all=False):
     n = len(ptsA)
     assert len(ptsB) == n
 
-    H_best = np.empty((3, 3), dtype='float32')
-    H_best[-1, :] = [0, 0, 1]
+    # initialize
+    H_best = np.zeros((3, 3), dtype='float32')
     outliers_best = len(ptsA)
     inliers = np.arange(0)
 
+    # outer loop: try num_iterations random permutations
     for _ in range(num_iterations):
         inds = np.random.permutation(range(n))
         i1, i2 = 0, s
-
         while i2 <= n:
+            # find homograpy matrix by least squares
             inds_in = inds[i1:i2]
-            src = np.hstack([ptsA[inds_in], np.ones((s, 1))])
-            dst = ptsB[inds_in]
-            H = np.linalg.lstsq(src, dst, rcond=None)[0]
+            H = estimate_homography(ptsA[inds_in], ptsB[inds_in])
 
+            # check the number of outliers
             inds_out = np.concatenate([inds[:i1], inds[i2:]])
-            src = np.hstack([ptsA[inds_out], np.ones((n - s, 1))])
-            dst = ptsB[inds_out]
-            distances = np.sqrt(np.sum((dst - src @ H)**2, axis=1))
+            pred = transform(ptsA[inds_out], H)
+            distances = np.sqrt(np.sum((ptsB[inds_out] - pred)**2, axis=1))
             mask = (distances <= reprojThresh)
             outliers_cur = (~mask).sum()
             if outliers_cur < outliers_best:
-                H_best[0:2, :] = H.T
+                H_best = H
                 outliers_best = outliers_cur
                 inliers = np.concatenate([inds_in, inds_out[mask]])
+
+            # update indices
             i1 = i2
             i2 = i1 + s
 
-    src = np.hstack([ptsA[inliers], np.ones((len(inliers), 1))])
-    H_best[0:2, :] = np.linalg.lstsq(src, ptsB[inliers], rcond=None)[0].T
+    # recalculate homography matrix using all inliers
+    if use_all:
+        H_best = estimate_homography(ptsA[inliers], ptsB[inliers])
     return H_best
+
+
+def estimate_homography(src, dst):
+    A = []
+    for (x1, y1), (x2, y2) in zip(src, dst):
+        A.append([-x1, -y1, -1, 0, 0, 0, x2 * x1, x2 * y1, x2])
+        A.append([0, 0, 0, -x1, -y1, -1, x1 * y2, y1 * y2, y2])
+    A = np.array(A)
+    _, _, v = np.linalg.svd(A)
+    return v[-1].reshape(3, 3).astype('float32')
+
+
+def transform(src, H):
+    inp = np.hstack([src, np.ones((len(src), 1))])
+    out = H @ inp.T
+    pred = np.zeros((len(src), 2), dtype='float32')
+    pred[:, 0] = out[0] / out[2]
+    pred[:, 1] = out[1] / out[2]
+    return pred
 
 
 if __name__ == '__main__':
