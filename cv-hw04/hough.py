6bf5f1acb75e8d649544f4bb434283c5dc84236a
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-04-21 19:04:33 +0300

    Hough circles

---
+++ cv-hw04/hough.py
@@ -0,0 +1,89 @@
+import os
+import copy
+import heapq
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+
+IMG_PATH = os.path.join(os.getcwd(), 'data', 'coins_1.jpg')
+
+
+def main():
+    img = cv2.imread(IMG_PATH)
+    blur = cv2.GaussianBlur(img, (7, 7), 1)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 180, 220)
+
+    # use implemented algorithm
+    A, radii = hough_transform(edges, 20, 60, 20)
+    circles = filter_hough_circles(A, radii, 55, 50)
+    print(f'Found {len(circles)} circles.')
+    img2 = copy.deepcopy(img)
+    for x, y, r in circles:
+        cv2.circle(img2, (x, y), r, color=(0, 255, 0), thickness=2)
+    plt.figure()
+    plt.imshow(img2)
+
+    # use cv2.HoughCircles
+    circles = cv2.HoughCircles(
+        gray,
+        cv2.HOUGH_GRADIENT,
+        dp=1,
+        minDist=40,
+        minRadius=20,
+        maxRadius=60,
+        param2=50
+    )
+    img3 = copy.deepcopy(img)
+    for circle in circles.squeeze():
+        x0, y0, r = np.uint16(circle)
+        cv2.circle(img3, (x0, y0), r, (0, 255, 0), thickness=2)
+    plt.figure()
+    plt.imshow(img3)
+
+    plt.show()
+
+
+def hough_transform(img, r_min=5, r_max=None, r_bins=30):
+    h, w = img.shape
+    if r_max is None:
+        r_max = min(h, w) // 2
+    radii = np.linspace(r_min, r_max, r_bins + 1)
+    x0, y0 = np.meshgrid(np.arange(0, w), np.arange(0, h))
+    A = np.zeros((h, w, r_bins), dtype='uint32')
+
+    for i in range(h):
+        print(f'Progress: {i}/{h - 1}', end='\r')
+        for j in range(w):
+            if img[i, j] == 0:
+                continue
+            r = np.sqrt((i - y0)**2 + (j - x0)**2)
+            for k in range(r_bins):
+                mask = (r >= radii[k]) & (r < radii[k + 1])
+                A[:, :, k][mask] += img[i, j]
+    print()
+
+    circ = np.pi * (radii[:-1] + radii[1:])
+    A = (A / (circ * 2.55))
+    return A, radii
+
+
+def filter_hough_circles(A, radii, threshold, min_dist=20):
+    heap = []
+    ix, iy, ir = np.where(A >= threshold)
+    for i, j, k in zip(ix, iy, ir):
+        heapq.heappush(heap, (-A[i, j, k], (j, i, radii[k])))
+
+    circles = []
+    for _, (x0, y0, r) in heap:
+        distances = [np.sqrt((xi - x0)**2 + (yi - y0)**2)
+                     for xi, yi, _ in circles]
+        if not distances or min(distances) > min_dist:
+            circles.append(np.array([x0, y0, r], dtype='uint16'))
+
+    return circles
+
+
+if __name__ == '__main__':
+    main()
