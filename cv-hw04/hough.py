a19bf53a4e6852c4ac47962b98fb9b5a558e275f
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-04-27 01:53:34 +0300

    Apply Hough transform to noisy image

--- cv-hw04/hough.py
+++ cv-hw04/hough.py
@@ -45,6 +45,14 @@ def main():
     plt.show()
 
 
+def hough_circles(img, threshold,
+                  canny_thresh_1=100, canny_thresh_2=200,
+                  r_min=5, r_max=None, r_bins=30, min_dist=20):
+    img = cv2.Canny(img, canny_thresh_1, canny_thresh_2)
+    A, radii = hough_transform(img, r_min, r_max, r_bins)
+    return filter_hough_circles(A, radii, threshold, min_dist)
+
+
 def hough_transform(img, r_min=5, r_max=None, r_bins=30):
     h, w = img.shape
     if r_max is None:
@@ -53,8 +61,9 @@ def hough_transform(img, r_min=5, r_max=None, r_bins=30):
     x0, y0 = np.meshgrid(np.arange(0, w), np.arange(0, h))
     A = np.zeros((h, w, r_bins), dtype='uint32')
 
+    # iterate over pixels
     for i in range(h):
-        print(f'Progress: {i}/{h - 1}', end='\r')
+        print(f'Hough transform progress: {i}/{h - 1}', end='\r')
         for j in range(w):
             if img[i, j] == 0:
                 continue
@@ -64,17 +73,20 @@ def hough_transform(img, r_min=5, r_max=None, r_bins=30):
                 A[:, :, k][mask] += img[i, j]
     print()
 
+    # scale accumulator matrix so that values are <= 100
     circ = np.pi * (radii[:-1] + radii[1:])
     A = (A / (circ * 2.55))
     return A, radii
 
 
 def filter_hough_circles(A, radii, threshold, min_dist=20):
+    # sort circles by "intensity"
     heap = []
     ix, iy, ir = np.where(A >= threshold)
     for i, j, k in zip(ix, iy, ir):
         heapq.heappush(heap, (-A[i, j, k], (j, i, radii[k])))
 
+    # remove circles that are too close to ones with higher intensity
     circles = []
     while heap:
         _, (x0, y0, r) = heapq.heappop(heap)
