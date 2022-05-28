65d6d8e6ea3435074895227bd50c784e1a54e824
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-05-28 19:45:32 +0300

    Add `train` function

--- dl-hw04/train_lstm.py
+++ dl-hw04/train_lstm.py
@@ -7,12 +7,14 @@ import torchtext.vocab as tvcb
 from torchtext.datasets import SST2
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pack_padded_sequence
+import matplotlib.pyplot as plt
 
 
 EMB_DIM = 300
 HIDDEN_SIZE = 300
 FC_SIZE = 400
-BATCH_SIZE = 50
+BATCH_SIZE = 128
+LEARNING_RATE = 1e-4
 EPOCHS = 25
 
 
@@ -20,12 +22,14 @@ def main():
     tokenizer = get_tokenizer('basic_english')
     embeddings_full = tvcb.GloVe('840B', dim=EMB_DIM)
     train_dataset = SST2(split='train')
-    vocab, embeddings = get_vocab_embeddings(train_dataset, tokenizer,
-                                             embeddings_full)
     train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE,
                                   shuffle=True)
     val_dataset = SST2(split='dev')
     val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
+
+    vocab, embeddings = get_vocab_embeddings(train_dataset, tokenizer,
+                                             embeddings_full)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     lstm = RNNClassifier(
         tokenizer=tokenizer,
@@ -38,42 +42,22 @@ def main():
         rnn_linear_dim=FC_SIZE
     )
     lstm.to(device)
-    loss_function = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(lstm.parameters())
-    lstm.train()
+    t_loss, t_acc, v_loss, v_acc = train(lstm, train_dataloader,
+                                         val_dataloader, device, EPOCHS)
 
-    for i in range(EPOCHS):
-        cur_loss = 0.0
-        hits = 0
-        total = 0
-        for texts, labels in train_dataloader:
-            X, y, lengths = lstm.text_to_tensor(texts, labels)
-            X, y = X.to(device), y.to(device)
-            logits = lstm(X, lengths)
-            predictions = torch.round(torch.sigmoid(logits.detach())).int()
-            hits += (predictions == y).sum().item()
-            total += len(y)
-            loss = loss_function(logits, y.float())
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            cur_loss += loss.item()
-        print(f'Epoch {i + 1}: loss = {cur_loss:.2f}')
-        print(f'Train accuracy = {hits * 100 / total:.1f}%')
-
-        lstm.eval()
-        hits = 0
-        total = 0
-        with torch.no_grad():
-            for texts, labels in val_dataloader:
-                X, y, lengths = lstm.text_to_tensor(texts, labels)
-                X, y = X.to(device), y.to(device)
-                logits = lstm(X, lengths)
-                predictions = torch.round(torch.sigmoid(logits)).int()
-                hits += (predictions == y).sum().item()
-                total += len(y)
-        print(f'Validation accuracy {(100 * hits / total):.1f}%\n')
-        lstm.train()
+    epochs = np.arange(1, EPOCHS + 1)
+    fig, [ax1, ax2] = plt.subplots(nrows=2, sharex=True)
+    fig.set_size_inches(6.0, 6.0)
+    ax1.plot(epochs, t_loss, label='train')
+    ax1.plot(epochs, v_loss, label='validate')
+    ax1.set_ylabel('Loss')
+    ax1.legend()
+    ax2.plot(epochs, t_acc, label='train')
+    ax2.plot(epochs, v_acc, label='validate')
+    ax2.set_ylabel('Accuracy')
+    ax2.set_xlabel('Epoch')
+    plt.tight_layout()
+    plt.show()
 
 
 class RNNClassifier(nn.Module):
@@ -183,6 +167,92 @@ def get_vocab_embeddings(dataset: Dataset,
         embeddings[i] = vectors[word]
 
     return vocab, embeddings
+
+
+def train(model: RNNClassifier, train_dataloader: DataLoader,
+          val_dataloader: DataLoader, device: torch.device, epochs: int,
+          load_best: bool = True, use_acc: bool = False,
+          display_progress: bool = True
+          ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    train_acc = np.zeros(epochs)
+    train_loss = np.zeros_like(train_acc)
+    val_acc = np.zeros_like(train_acc)
+    val_loss = np.ones_like(train_acc) * np.inf
+    if load_best:
+        state_dict_best = model.state_dict()
+
+    loss_function = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    for i in range(epochs):
+        # train loop
+        model.train()
+        hits = 0   # accurate predictions
+        total = 0  # number of input texts
+        for texts, labels in train_dataloader:
+            # transform text to tensor of tokens with padding
+            X, y, lengths = model.text_to_tensor(texts, labels)
+            X, y = X.to(device), y.to(device)
+
+            # run forward loop
+            logits = model(X, lengths)
+            loss = loss_function(logits, y.float())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # update accuracy and loss
+            hits += accurate_predictions(logits.detach(), y)
+            total += len(y)
+            train_loss[i] += loss.item() * len(y)  # avg loss per sample
+
+        train_acc[i] = hits / total
+        train_loss[i] /= total
+
+        # validation
+        val_acc[i], val_loss[i] = evaluate(model, val_dataloader, device)
+
+        if display_progress:
+            print(f'Epoch {i + 1}')
+            print('train: loss = {:.2e}, accuracy = {:.2f}%'.format(
+                train_loss[i], train_acc[i] * 100))
+            print('validate: loss = {:.2e}, accuracy = {:.2f}%'.format(
+                val_loss[i], val_acc[i] * 100))
+            print()
+
+        if load_best:
+            best_ind = np.argmax(val_acc) if use_acc else np.argmin(val_loss)
+            if best_ind == i:
+                state_dict_best = model.state_dict()
+
+    if load_best:  # load model with best validation accuracy
+        model.load_state_dict(state_dict_best)
+
+    return train_loss, train_acc, val_loss, val_acc
+
+
+@torch.no_grad()
+def evaluate(model: RNNClassifier, dataloader: DataLoader, device: torch.device
+             ) -> tuple[float, float]:
+    model.eval()
+    hits, total = 0, 0
+    total_loss = 0.0
+    loss_function = nn.BCEWithLogitsLoss()
+
+    for texts, labels in dataloader:
+        X, y, lengths = model.text_to_tensor(texts, labels)
+        X, y = X.to(device), y.to(device)
+    logits = model(X, lengths)
+    total_loss += loss_function(logits, y.float()).item() * len(y)
+    hits += accurate_predictions(logits, y)
+    total += len(y)
+
+    return hits / total, total_loss / total
+
+
+def accurate_predictions(logits: torch.tensor,
+                         true_labels: torch.tensor) -> float:
+    predictions = torch.round(torch.sigmoid(logits)).int()
+    return (predictions == true_labels).sum().item()
 
 
 if __name__ == '__main__':
