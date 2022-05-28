10b73291fc0b98d0ba7010e34bc26260c1174c23
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-05-28 17:57:15 +0300

    Create class for generic RNNs

--- dl-hw04/train_lstm.py
+++ dl-hw04/train_lstm.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Union
 import numpy as np
 import torch
 from torch import nn
@@ -11,9 +11,9 @@ from torch.nn.utils.rnn import pack_padded_sequence
 
 EMB_DIM = 300
 HIDDEN_SIZE = 300
-FC_SIZE = 4 * HIDDEN_SIZE
-BATCH_SIZE = 256
-EPOCHS = 50
+FC_SIZE = 400
+BATCH_SIZE = 50
+EPOCHS = 25
 
 
 def main():
@@ -27,17 +27,28 @@ def main():
     val_dataset = SST2(split='dev')
     val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    lstm = LSTM(tokenizer, vocab, embeddings, device)
+    lstm = RNNClassifier(
+        tokenizer=tokenizer,
+        vocab=vocab,
+        input_size=EMB_DIM,
+        hidden_size=HIDDEN_SIZE,
+        embeddings=embeddings,
+        rnn='LSTM',
+        lstm_bidirectional=True,
+        rnn_linear_dim=FC_SIZE
+    )
+    lstm.to(device)
     loss_function = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(lstm.parameters())
     lstm.train()
 
-    for i in range(30):
+    for i in range(EPOCHS):
         cur_loss = 0.0
         hits = 0
         total = 0
         for texts, labels in train_dataloader:
-            X, y, lengths = lstm.texts_to_tensor(texts, labels)
+            X, y, lengths = lstm.text_to_tensor(texts, labels)
+            X, y = X.to(device), y.to(device)
             logits = lstm(X, lengths)
             predictions = torch.round(torch.sigmoid(logits.detach())).int()
             hits += (predictions == y).sum().item()
@@ -55,7 +66,8 @@ def main():
         total = 0
         with torch.no_grad():
             for texts, labels in val_dataloader:
-                X, y, lengths = lstm.texts_to_tensor(texts, labels)
+                X, y, lengths = lstm.text_to_tensor(texts, labels)
+                X, y = X.to(device), y.to(device)
                 logits = lstm(X, lengths)
                 predictions = torch.round(torch.sigmoid(logits)).int()
                 hits += (predictions == y).sum().item()
@@ -64,39 +76,82 @@ def main():
         lstm.train()
 
 
-class LSTM(nn.Module):
+class RNNClassifier(nn.Module):
     def __init__(self, tokenizer: Callable, vocab: tvcb.Vocab,
-                 embeddings: np.ndarray, device: torch.device):
+                 input_size: int, hidden_size: Union[int, None] = None,
+                 embeddings: Union[np.ndarray, torch.tensor, None] = None,
+                 rnn: str = 'LSTM', lstm_bidirectional: bool = False,
+                 rnn_linear_dim: Union[int, None] = None):
         super().__init__()
-        self.input_size = embeddings.shape[1]
+        assert embeddings is None or input_size == embeddings.shape[1]
+        self.input_size = input_size
+        if hidden_size is None:
+            hidden_size = self.input_size
+        self.hidden_size = hidden_size
+
+        # text processing tools
         self.tokenizer = tokenizer
         self.vocab = vocab
-        self.device = device
-        self.embedding = nn.Embedding.from_pretrained(
-            torch.tensor(embeddings, dtype=torch.float32),
-            freeze=True
-        ).to(device)
-        self.rnn = nn.LSTM(
+        if embeddings is None:
+            self.embedding = nn.Embedding(
+                len(vocab), hidden_size, padding_idx=0
+            )
+        else:
+            if isinstance(embeddings, np.ndarray):
+                embeddings = torch.tensor(embeddings).float()
+            self.embedding = nn.Embedding.from_pretrained(
+                embeddings=embeddings,
+                freeze=True
+            )
+
+        # recurrent block
+        rnn_settings = dict(
             input_size=self.input_size,
-            hidden_size=HIDDEN_SIZE,
+            hidden_size=self.hidden_size,
             batch_first=True,
-            bidirectional=False
-        ).to(device)
+        )
+        self.rnn_type = rnn
+        self.is_bidirectional = False  # defines rnn "output" size
+        if rnn == 'LSTM':
+            rnn_module = nn.LSTM
+            if lstm_bidirectional:
+                rnn_settings['bidirectional'] = lstm_bidirectional
+                self.is_bidirectional = True
+        elif rnn == 'RNN':
+            rnn_module = nn.RNN
+        elif rnn == 'GRU':
+            rnn_module = nn.GRU
+        else:
+            raise ValueError(f'Unrecoginezed RNN type {rnn}')
+        self.rnn = rnn_module(**rnn_settings)
+
+        # linear layers after RNN
+        if self.rnn_type == 'LSTM' and lstm_bidirectional:
+            in_features = hidden_size * 2
+        else:
+            in_features = hidden_size
+        if rnn_linear_dim is None:
+            rnn_linear_dim = in_features
         self.stack = nn.Sequential(
-            nn.Linear(HIDDEN_SIZE, FC_SIZE),
+            nn.Linear(in_features, rnn_linear_dim),
             nn.ReLU(),
-            nn.Linear(FC_SIZE, 1)
-        ).to(device)
+            nn.Linear(rnn_linear_dim, 1)  # binary classifier
+        )
 
     def forward(self, X: torch.tensor, lengths: torch.tensor):
         X = self.embedding(X)
         X = pack_padded_sequence(X, lengths, batch_first=True,
                                  enforce_sorted=False)
-        _, (h, _) = self.rnn(X)
-        logits = self.stack(h)
+        _, hidden = self.rnn(X)
+        if self.rnn_type == 'LSTM':
+            hidden = hidden[0]
+            if self.is_bidirectional:
+                hidden = torch.cat((hidden[0], hidden[1]), axis=1)
+        logits = self.stack(hidden)
         return logits.squeeze()
 
-    def texts_to_tensor(self, texts: tuple[str], labels: torch.tensor):
+    def text_to_tensor(self, texts: tuple[str], labels: torch.tensor
+                       ) -> tuple[torch.tensor, torch.tensor, torch.tensor]:
         tokens = [self.vocab(self.tokenizer(text)) for text in texts]
         lengths = torch.tensor([len(token_list) for token_list in tokens],
                                dtype=torch.int64)
@@ -106,8 +161,8 @@ class LSTM(nn.Module):
         for i, ind in enumerate(perm_inds):
             j = len(tokens[ind])
             tokens_arr[i, :j] = np.asarray(tokens[ind])
-        X = torch.tensor(tokens_arr, device=self.device)
-        y = labels[perm_inds].to(self.device)
+        X = torch.tensor(tokens_arr)
+        y = labels[perm_inds]
         return X, y, lengths
 
 
